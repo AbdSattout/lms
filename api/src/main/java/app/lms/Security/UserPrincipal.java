c73d2848afd7fa2768123e2aaa1842426aa3c8a8
AbodehAlshammaa <abd.alshammaa05@gmail.com>
2026-05-06 15:05:03 +0300

    cleaning code and fixing telegramJwtDecoder

--- api/src/main/java/app/lms/Security/UserPrincipal.java
+++ api/src/main/java/app/lms/Security/UserPrincipal.java
@@ -1,6 +1,7 @@
 package app.lms.Security;
 
 import app.lms.model.User;
+import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,15 +9,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-public class UserPrincipal implements UserDetails {
-
-    private final User user;
-
-    public UserPrincipal(User user){
-        this.user = user;
-    }
+public record UserPrincipal(User user) implements UserDetails {
 
     @Override
+    @NullMarked
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
     }
@@ -27,30 +23,33 @@ public class UserPrincipal implements UserDetails {
     }
 
     @Override
+    @NullMarked
     public String getUsername() {
         return user.getTelegramId();
     }
+
     @Override
+    @NullMarked
     public boolean isAccountNonExpired() {
         return true;
     }
 
     @Override
+    @NullMarked
     public boolean isAccountNonLocked() {
         return true;
     }
 
     @Override
+    @NullMarked
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
     @Override
+    @NullMarked
     public boolean isEnabled() {
         return true;
     }
 
-    public User getUser() {
-        return user;
-    }
 }
