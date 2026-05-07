e1b1f74e03bd4dad719b86efa1a19439ec97e2b1
Sahl <sahelalobid1@gmail.com>
2026-05-07 10:52:48 +0300

    feat(auth): implement telegram oidc login

---
+++ mobile/lib/core/services/injection_container.dart
@@ -0,0 +1,56 @@
+import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// Import Core
+import '../connection/network_info.dart';
+import '../databases/api/api_consumer.dart';
+import '../databases/api/dio_consumer.dart';
+import '../databases/cache/cache_helper.dart';
+
+// Import Auth Feature
+import 'package:lms/features/auth/data/datasources/auth_local_datasource.dart';
+import 'package:lms/features/auth/data/datasources/auth_remote_datasource.dart';
+import '../../features/auth/data/repositories/auth_repository_impl.dart';
+import '../../features/auth/domain/repositories/auth_repository.dart';
+import '../../features/auth/domain/usecases/login_with_telegram.dart';
+
+// This is the service locator instance
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Features - Auth
+  
+  // Use cases
+  sl.registerLazySingleton(() => LoginWithTelegram(repository: sl()));
+
+  // Repository
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // Data sources
+  sl.registerLazySingleton(() => AuthRemoteDataSource(appAuth: sl()));
+  sl.registerLazySingleton(() => AuthLocalDataSource(cache: sl()));
+
+  //! Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+  sl.registerLazySingleton<ApiConsumer>(() => DioConsumer(dio: sl()));
+  sl.registerLazySingleton(() => CacheHelper());
+
+  //! External
+  
+  // SharedPreferences initialization
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+
+  sl.registerLazySingleton(() => Dio());
+  sl.registerLazySingleton(() => const FlutterAppAuth());
+  sl.registerLazySingleton(() => DataConnectionChecker());
+}
---
+++ mobile/lib/features/auth/data/datasources/auth_local_datasource.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import '../../../../core/databases/cache/cache_helper.dart';
+import 'package:lms/core/errors/exceptions.dart';
+import '../models/auth_model.dart';
+
+class AuthLocalDataSource {
+  final CacheHelper cache;
+  final String key = "CachedAuthToken";
+
+  AuthLocalDataSource({required this.cache});
+
+  cacheAuthData(AuthModel? authToCache) {
+    if (authToCache != null) {
+      cache.saveData(
+        key: key,
+        value: json.encode(authToCache.toJson()),
+      );
+    } else {
+      throw CacheExeption(errorMessage: "No data to cache");
+    }
+  }
+
+  Future<AuthModel> getCachedAuthData() {
+    final jsonString = cache.getDataString(key: key);
+
+    if (jsonString != null) {
+      return Future.value(AuthModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheExeption(errorMessage: "No cached token found");
+    }
+  }
+}
