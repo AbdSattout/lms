ed929e91cd6acecde9f8d08af292879b7b8f2e0d
ali <lghdxhuzlh@gmail.com>
2026-05-19 22:31:06 +0300

    Update Profile Security

--- api/src/main/java/app/lms/controller/ProfileController.java
+++ api/src/main/java/app/lms/controller/ProfileController.java
@@ -3,9 +3,11 @@ package app.lms.controller;
 
 import app.lms.dto.ProfileResponse;
 import app.lms.dto.UpdateProfile;
+import app.lms.security.UserPrincipal;
 import app.lms.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,25 +18,29 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @GetMapping("/me")
-    public ResponseEntity<ProfileResponse> getMyProfile() {
+    public ResponseEntity<ProfileResponse> getMyProfile(
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+            ) {
         return ResponseEntity.ok(
-                profileService.getMyProfile()
+                profileService.getMyProfile(userPrincipal.user())
         );
     }
 
     @PatchMapping("/me")
     public ResponseEntity<ProfileResponse> updateProfile(
-            @RequestBody UpdateProfile request
+            @RequestBody UpdateProfile request , @AuthenticationPrincipal UserPrincipal userPrincipal
     ) {
 
         return ResponseEntity.ok(
-                profileService.updateProfile(request)
+                profileService.updateProfile(request , userPrincipal.user())
         );
     }
 
     @DeleteMapping("/me")
-    public ResponseEntity<?> deleteProfile() {
-        profileService.deleteProfile();
+    public ResponseEntity<?> deleteProfile(
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+    ) {
+        profileService.deleteProfile(userPrincipal.user());
         return ResponseEntity.ok().body(
                 "Profile deleted"
         );
