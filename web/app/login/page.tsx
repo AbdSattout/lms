697c2b4dbe5dab144dde8fda00d08d860a4ae58c
Abd Sattout <abdsat@duck.com>
2026-05-22 20:36:54 +0300

    wrap `useSearchParams` in a suspense

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -1,11 +1,16 @@
+import { Suspense } from "react"
+
 import { LoginButton } from "@/components/auth/login-button"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function LoginPage() {
   return (
     <main className="flex min-h-dvh items-center justify-center">
-      <div className="flex flex-col items-center gap-4">
+      <div className="flex min-w-60 flex-col items-center gap-4">
         <h1 className="mb-3 font-heading text-4xl">تسجيل الدخول</h1>
-        <LoginButton />
+        <Suspense fallback={<Skeleton className="h-10 w-full rounded-none" />}>
+          <LoginButton />
+        </Suspense>
       </div>
     </main>
   )
