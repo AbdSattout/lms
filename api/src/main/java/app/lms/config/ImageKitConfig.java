a19b5c2547878716fe976cf7f9b3a2003ade700f
AbodehAlshammaa <abd.alshammaa05@gmail.com>
2026-05-22 21:11:54 +0300

    resolve test failurs caused by missing env vars

--- api/src/main/java/app/lms/config/ImageKitConfig.java
+++ api/src/main/java/app/lms/config/ImageKitConfig.java
@@ -2,15 +2,20 @@ package app.lms.config;
 
 import io.imagekit.client.ImageKitClient;
 import io.imagekit.client.okhttp.ImageKitOkHttpClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ImageKitConfig {
+    private final ImageKitProperties properties;
 
     @Bean
     public ImageKitClient imageKitClient() {
 
-        return ImageKitOkHttpClient.fromEnv();
+        return ImageKitOkHttpClient.builder()
+                .privateKey(properties.getPrivateKey())
+                .build();
     }
 }
