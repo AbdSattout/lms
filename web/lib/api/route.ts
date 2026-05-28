37f45b1477b0c8fe4b83c101db2e16640bfa7896
Abd Sattout <abdsat@duck.com>
2026-05-28 21:48:54 +0300

    make get handler optional in api routes

--- web/lib/api/route.ts
+++ web/lib/api/route.ts
@@ -2,21 +2,23 @@ import "server-only"
 
 type ApiMethod = (...args: never[]) => Promise<unknown>
 
-type ApiMethods = {
-  get: ApiMethod
-} & Partial<Record<"post" | "patch" | "delete", ApiMethod>>
+type ApiMethods = Partial<
+  Record<"get" | "post" | "patch" | "delete", ApiMethod>
+>
 
-export type ApiRoute<T extends ApiMethods> = T & {
-  (...args: Parameters<T["get"]>): ReturnType<T["get"]>
-}
+export type ApiRoute<T extends ApiMethods> = T &
+  (T["get"] extends ApiMethod
+    ? (...args: Parameters<T["get"]>) => ReturnType<T["get"]>
+    : unknown)
 
 export type ApiTree = {
   [key: string]: ApiRoute<ApiMethods> | ApiTree
 }
 
 export function defineApiRoute<T extends ApiMethods>(methods: T): ApiRoute<T> {
+  if (!methods.get) return methods as ApiRoute<T>
   return Object.assign(
-    (...args: Parameters<T["get"]>) => methods.get(...args),
+    (...args: Parameters<NonNullable<T["get"]>>) => methods.get!(...args),
     methods
   ) as ApiRoute<T>
 }
