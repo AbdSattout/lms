f935eb1f85c2d424d67714e50b56a3a5ea18685a
ali <lghdxhuzlh@gmail.com>
2026-05-30 17:48:54 +0300

    edit course

--- api/src/main/java/app/lms/courceEnrollment/controller/CourseEnrollmentController.java
+++ api/src/main/java/app/lms/courceEnrollment/controller/CourseEnrollmentController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,5 +35,22 @@ public class CourseEnrollmentController {
                                 principal.user()
                         )
                 );
+    }
+
+    @DeleteMapping("/{courseId}/enroll")
+    public ResponseEntity<Void> unenroll(
+
+            @PathVariable Long courseId,
+
+            @AuthenticationPrincipal
+            UserPrincipal principal
+    ) {
+
+        enrollmentService.unenroll(
+                courseId,
+                principal.user()
+        );
+
+        return ResponseEntity.noContent().build();
     }
 }
