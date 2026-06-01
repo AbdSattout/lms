b735153daee422c307a6b4334d2327a371de2e92
AbodehAlshammaa <abd.alshammaa05@gmail.com>
2026-06-01 11:33:35 +0300

    adding update to slug

--- api/src/main/java/app/lms/organization/dto/UpdateOrganizationRequest.java
+++ api/src/main/java/app/lms/organization/dto/UpdateOrganizationRequest.java
@@ -1,6 +1,7 @@
 package app.lms.organization.dto;
 
 import app.lms.organization.emums.Visibility;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -10,10 +11,15 @@ public class UpdateOrganizationRequest {
     @Size(max = 100)
     private String name;
 
+
+    @Pattern(
+            regexp = "^[a-z0-9-]+$",
+            message = "Invalid slug"
+    )
+    private String slug;
+
     @Size(max = 5000)
     private String description;
-
-    private String image;
 
     private Visibility visibility;
 }
