b483ed6bfe2d8eabf8b71ea479c92bf8eaa9428a
Karam-Nssr <karamnssr@gmail.com>
2026-06-08 22:46:33 +0300

    profile feature modifications

--- mobile/lib/features/profile/presentation/bloc/profile_event.dart
+++ mobile/lib/features/profile/presentation/bloc/profile_event.dart
@@ -1,10 +1,8 @@
 abstract class ProfileEvent {}
 
-class GetProfileEvent
-    extends ProfileEvent {}
+class GetProfileEvent extends ProfileEvent {}
 
-class UpdateProfilePictureEvent
-    extends ProfileEvent {
+class UpdateProfilePictureEvent extends ProfileEvent {
   final String imagePath;
 
   UpdateProfilePictureEvent(
