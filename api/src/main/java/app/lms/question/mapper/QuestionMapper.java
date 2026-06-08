d1b026ebca34d7ff441e74450c3720ea950d47cd
AbodehAlshammaa <abd.alshammaa05@gmail.com>
2026-06-08 16:58:07 +0300

    question package

---
+++ api/src/main/java/app/lms/question/mapper/QuestionMapper.java
@@ -0,0 +1,33 @@
+package app.lms.question.mapper;
+
+import app.lms.question.dto.QuestionPublicResponse;
+import app.lms.question.dto.QuestionResponse;
+import app.lms.question.model.Question;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuestionMapper {
+
+    public QuestionResponse toResponse(
+            Question question
+    ) {
+
+        return new QuestionResponse(
+                question.getId(),
+                question.getContent(),
+                question.getOptions(),
+                question.getCorrectAnswerIndex()
+        );
+    }
+
+    public QuestionPublicResponse toPublicResponse(
+            Question question
+    ) {
+
+        return new QuestionPublicResponse(
+                question.getId(),
+                question.getContent(),
+                question.getOptions()
+        );
+    }
+}
