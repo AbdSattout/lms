8c1822d560b99f11a8ec1ab40f859d71aa024e00
Abd Sattout <abdsat@duck.com>
2026-06-11 06:19:25 +0300

    add generateSlug util

--- web/lib/utils.ts
+++ web/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateSlug(name: string): string | null {
+  if (!name || !/^[a-zA-Z\s_]+$/.test(name)) return `org-${Date.now()}`
+  return name
+    .trim()
+    .toLowerCase()
+    .replace(/[\s_]+/g, "-")
+}
