88db394f19ef9d96e26bd9a6a14874c36ca7d9a5
Abd Sattout <abdsat@duck.com>
2026-06-22 09:22:42 +0300

    replace cn with cnfast

--- web/lib/utils.ts
+++ web/lib/utils.ts
@@ -1,15 +1,1 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-export function generateSlug(name: string, prefix = "org"): string | null {
-  if (!name) return null
-  if (!/^[a-zA-Z\s_]+$/.test(name)) return `${prefix}-${Date.now()}`
-  return name
-    .trim()
-    .toLowerCase()
-    .replace(/[\s_]+/g, "-")
-}
+export { cn, type ClassValue } from "cnfast";
