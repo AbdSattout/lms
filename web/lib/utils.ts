f192be9cb141dd02532d303b5e1e64a83be6b8da
Abd Sattout <abdsat@duck.com>
2026-06-29 19:25:41 +0300

    restore generateSlug

--- web/lib/utils.ts
+++ web/lib/utils.ts
@@ -1,1 +1,10 @@
 export { cn, type ClassValue } from "cnfast";
+
+export function generateSlug(name: string, prefix = "org"): string | null {
+  if (!name) return null
+  if (!/^[a-zA-Z\s_]+$/.test(name)) return `${prefix}-${Date.now()}`
+  return name
+    .trim()
+    .toLowerCase()
+    .replace(/[\s_]+/g, "-")
+}
