03ea9f9274bc86860ca21dccc8ca14e3a7306066
unknown <eiadkareme@gmail.com>
2026-07-09 00:11:29 +0300

    final

--- web/next.config.ts
+++ web/next.config.ts
@@ -6,8 +6,6 @@ const imageKitUrl =
 const nextConfig: NextConfig = {
   reactCompiler: true,
   typedRoutes: true,
-  allowedDevOrigins: ["127.0.0.1", "localhost:3000"],
-  skipProxyUrlNormalize: true,
   images: {
     remotePatterns: [
       new URL("https://t.me/i/userpic/**"),
