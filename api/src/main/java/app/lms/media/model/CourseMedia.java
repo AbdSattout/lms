42b63b4d9c596e555b9c5d43c3efcb1ce4fe9529
AbodehAlshammaa <abd.alshammaa05@gmail.com>
2026-07-11 14:46:53 +0300

    adding constraints to courseMedia model

--- api/src/main/java/app/lms/media/model/CourseMedia.java
+++ api/src/main/java/app/lms/media/model/CourseMedia.java
@@ -21,20 +21,23 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class CourseMedia extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String url;
 
+    @Column(nullable = false)
     private String fileId;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private FileType type;
 
     @ManyToOne(fetch = FetchType.LAZY)
