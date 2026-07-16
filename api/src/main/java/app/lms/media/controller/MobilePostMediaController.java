322ece33a98e774ac8e76c4c6c002c5119b001fc
AbodehAlshammaa <abd.alshammaa05@gmail.com>
2026-07-16 15:54:55 +0300

    seperating to dashboard and mobile and adding all slugs

---
+++ api/src/main/java/app/lms/media/controller/MobilePostMediaController.java
@@ -0,0 +1,36 @@
+package app.lms.media.controller;
+
+import app.lms.media.dto.PostMediaResponse;
+import app.lms.media.service.MobilePostMediaService;
+import app.lms.security.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mobile/organizations/{slug}/post-media")
+@RequiredArgsConstructor
+public class MobilePostMediaController {
+
+    private final MobilePostMediaService postMediaService;
+
+    @GetMapping("/{mediaId}")
+    public ResponseEntity<PostMediaResponse> getById(
+            @PathVariable String slug,
+            @PathVariable Long mediaId,
+            @AuthenticationPrincipal UserPrincipal principal
+    ) {
+
+        return ResponseEntity.ok(
+                postMediaService.getById(
+                        slug,
+                        mediaId,
+                        principal.user()
+                )
+        );
+    }
+}
