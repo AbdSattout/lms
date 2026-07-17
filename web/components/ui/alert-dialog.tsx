9e27df3dd6433406a81ac34e0dd46e26e2fbc329
Abd Sattout <abdsat@duck.com>
2026-07-17 14:57:04 +0300

    fix: backdrop for nested dialogs

--- web/components/ui/alert-dialog.tsx
+++ web/components/ui/alert-dialog.tsx
@@ -24,6 +24,7 @@ function AlertDialogPortal({ ...props }: AlertDialogPrimitive.Portal.Props) {
 
 function AlertDialogOverlay({
   className,
+  forceRender = true,
   ...props
 }: AlertDialogPrimitive.Backdrop.Props) {
   return (
@@ -33,6 +34,7 @@ function AlertDialogOverlay({
         "fixed inset-0 isolate z-50 bg-black/30 duration-100 supports-backdrop-filter:backdrop-blur-sm data-open:animate-in data-open:fade-in-0 data-closed:animate-out data-closed:fade-out-0",
         className
       )}
+      forceRender={forceRender}
       {...props}
     />
   )
