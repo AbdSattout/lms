7f6a5152dfbe16bbb596915d85f6fb8dc15d909d
unknown <eiadkareme@gmail.com>
2026-08-07 23:13:36 +0300

    oops

--- web/next.config.ts
+++ web/next.config.ts
@@ -7,8 +7,6 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   typedRoutes: true,
   cacheComponents: true,
-  allowedDevOrigins: ["127.0.0.1", "localhost:3000"],
-  skipProxyUrlNormalize: true,
   images: {
     remotePatterns: [
       new URL("https://t.me/i/userpic/**"),
