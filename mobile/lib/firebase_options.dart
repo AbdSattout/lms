c95fe191c572baf5cf3123175fa274e530e6ba8c
AbodehAlshammaa <abd.alshammaa05@gmail.com>
2026-08-10 00:05:20 +0300

    Add Android Firebase configuration

--- mobile/lib/firebase_options.dart
+++ mobile/lib/firebase_options.dart
@@ -24,16 +24,27 @@ class DefaultFirebaseOptions {
     );
   }
 
-  static const _apiKey = String.fromEnvironment('FIREBASE_API_KEY');
-  static const _appId = String.fromEnvironment('FIREBASE_APP_ID');
+  static const _apiKey = String.fromEnvironment(
+    'FIREBASE_API_KEY',
+    defaultValue: 'AIzaSyDed8_Kscg2iu1EY4uYeq3yf_gkZGP5Fgs',
+  );
+  static const _appId = String.fromEnvironment(
+    'FIREBASE_APP_ID',
+    defaultValue: '1:304854141630:android:638c76e5ec2359bdf16cef',
+  );
   static const _messagingSenderId = String.fromEnvironment(
     'FIREBASE_MESSAGING_SENDER_ID',
+    defaultValue: '304854141630',
   );
-  static const _projectId = String.fromEnvironment('FIREBASE_PROJECT_ID');
+  static const _projectId = String.fromEnvironment(
+    'FIREBASE_PROJECT_ID',
+    defaultValue: 'masar-lms',
+  );
   static const _authDomain = String.fromEnvironment('FIREBASE_AUTH_DOMAIN');
   static const _databaseUrl = String.fromEnvironment('FIREBASE_DATABASE_URL');
   static const _storageBucket = String.fromEnvironment(
     'FIREBASE_STORAGE_BUCKET',
+    defaultValue: 'masar-lms.firebasestorage.app',
   );
   static const _measurementId = String.fromEnvironment(
     'FIREBASE_MEASUREMENT_ID',
