12293abf2abbb741ba7a94ff63e415f62415f591
AbodehAlshammaa <abd.alshammaa05@gmail.com>
2026-08-20 18:51:26 +0300

    test chat after editing

--- api/src/main/java/app/lms/chat/service/MessageService.java
+++ api/src/main/java/app/lms/chat/service/MessageService.java
@@ -58,11 +58,11 @@ public class MessageService {
                         );
 
         if (conversation.getType() == ConversationType.DIRECT) {
-            Long otherUserId = conversation.getDirectUserOne().getId().equals(user.getId())
-                    ? conversation.getDirectUserTwo().getId()
-                    : conversation.getDirectUserOne().getId();
 
-            friendService.validateIsFriends(user.getId(), otherUserId);
+            friendService.validateIsFriends(
+                    conversation.getDirectUserOne().getId(),
+                    conversation.getDirectUserTwo().getId()
+            );
         }
 
         chatMuteService.validateCanSendMessage(
@@ -99,6 +99,7 @@ public class MessageService {
 
         return response;
     }
+
 
     @Transactional(readOnly = true)
     public Page<MessageResponse> getMessages(
